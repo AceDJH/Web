09984a2e1fe45f71b2fdd4add6f9d84f6e1b77bf
AceDJH <460745177@qq.com>
2020-03-05 18:30:40 +0800

    Cookie

---
+++ servlet/src/session/CookieDemo1.java
@@ -0,0 +1,25 @@
+package session;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Author AceDJH
+ * @Date 2020/3/5 10:18
+ */
+@WebServlet("/cookieDemo1")
+public class CookieDemo1 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Cookie cookie = new Cookie("msg", "hello");
+        response.addCookie(cookie);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
---
+++ servlet/src/session/CookieDemo2.java
@@ -0,0 +1,32 @@
+package session;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Author AceDJH
+ * @Date 2020/3/5 10:20
+ */
+@WebServlet("/cookieDemo2")
+public class CookieDemo2 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 获取cookie
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null){
+            for (Cookie cookie : cookies) {
+                String name = cookie.getName();
+                String value = cookie.getValue();
+                System.out.println(name + " : " + value);
+            }
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
